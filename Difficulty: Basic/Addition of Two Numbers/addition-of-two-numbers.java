19210c026c3139a97755f8fb9854d11bbf7f277f
Mohan Kumar <63906309+MohanKumarD-tech@users.noreply.github.com>
2025-10-08 12:30:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Addition of Two Numbers/addition-of-two-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long addition(long a, long b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        long A = 10, B = 20;
+        Solution sum = new Solution();
+        System.out.println(sum.addition(A, B));
+    }
+}
