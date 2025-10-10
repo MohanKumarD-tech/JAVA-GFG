4c913680563e2bab33ad22b84f5188fb895ff21e
Mohan Kumar <63906309+MohanKumarD-tech@users.noreply.github.com>
2025-10-10 14:05:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -1,25 +1,16 @@
-// User function Template for Java
 class Solution {
     static boolean armstrongNumber(int n) {
-        // code here
-        // Extract digits
-        int hundreds = n / 100;
-        int tens = (n / 10) % 10;
-        int ones = n % 10;
-        
-        // Calculate the sum of cubes of digits
-        int armstrongSum = (int) (Math.pow(hundreds, 3) + Math.pow(tens, 3) + Math.pow(ones, 3));
-        
-        // Check if the sum is equal to the original number
-        return armstrongSum == n;
+        int sum = 0, temp = n, digits = String.valueOf(n).length();
+        while (temp > 0) {
+            sum += Math.pow(temp % 10, digits);
+            temp /= 10;
+        }
+        return sum == n;
     }
 
     public static void main(String[] args) {
-        int[] testCases = {153, 372, 100};
-        
-        // Check for each test case
-        for (int n : testCases) {
-            System.out.println("Is " + n + " an Armstrong number? " + armstrongNumber(n));
-        }
+        int[] testCases = {153, 372, 100, 9474};  // 9474 is a 4-digit Armstrong number
+        for (int n : testCases)
+            System.out.println(n + ": " + armstrongNumber(n));
     }
 }
