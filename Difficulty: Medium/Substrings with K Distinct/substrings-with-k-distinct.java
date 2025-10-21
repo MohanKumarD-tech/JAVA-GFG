5bb1c5d138e06394e9c6d77e6eba1ad543b3a019
Mohan Kumar <63906309+MohanKumarD-tech@users.noreply.github.com>
2025-10-21 13:16:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int countSubstr(String s, int k) {
+        int start=0,end=0,n=s.length(),count=0,subs=0;
+        int arr[]=new int[26];
+        while(end<n)
+        {
+            char c=s.charAt(end);
+            int i=c-'a';
+            if(arr[i]==0)
+            {
+                arr[i]++;
+                count++;
+                end++;
+            }
+            else if(arr[i]>0)
+            {
+                arr[i]++;
+                end++;
+            }
+            if(k==count)
+                subs++;
+            else if(count>k)
+            {
+                Arrays.fill(arr,0);
+                start++;
+                count=0;
+                end=start;
+            }
+            if(end==n && start<end)
+            {
+                Arrays.fill(arr,0);
+                start++;
+                end=start;
+                count=0;
+            }
+        }
+        return subs;
+    }
+}
